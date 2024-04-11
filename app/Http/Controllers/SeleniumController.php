0980b0e12c0bf92b0197b2a025291c6dc4bfd58e
david <bran.david@vlinde.com>
2024-04-11 21:12:47 +0300

    refactoring demo betano data

--- app/Http/Controllers/SeleniumController.php
+++ app/Http/Controllers/SeleniumController.php
@@ -11,11 +11,11 @@ use Facebook\WebDriver\WebDriverWait;
 
 class SeleniumController extends Controller
 {
+    private const BETANO_LIG1 = "https://ro.betano.com/sport/fotbal/romania/liga-1/17088/";
+    private const SERVER_SELENIUM_URL = "http://selenium:4444/wd/hub"; // Adresa Selenium Server
+
     public function fetchData()
     {
-        // Adresa Selenium Server
-        $serverUrl = 'http://selenium:4444/wd/hub';
-
         // Creare opțiuni pentru Firefox
         $firefoxOptions = new FirefoxOptions();
         $firefoxOptions->addArguments(['--headless']); // Rulează Firefox în mod headless (fără interfață grafică)
@@ -25,44 +25,46 @@ class SeleniumController extends Controller
         $capabilities->setCapability('moz:firefoxOptions', $firefoxOptions->toArray());
 
         try {
-            // Creare driver WebDriver pentru Firefox
-            $driver = RemoteWebDriver::create($serverUrl, $capabilities);
+            $betanoMatches = $this->scrapeDemoBetanoMatches($capabilities);
+            return view('selenium', compact("betanoMatches"));
+        } catch (\Exception $e) {
+            dd($e);
+        }
+    }
+    private function scrapeDemoBetanoMatches($capabilities, $waitTimeout = 10, $waitPresenceTimeout = 5)
+    {
+        // Creare driver WebDriver pentru Firefox
+        $driver = RemoteWebDriver::create(self::SERVER_SELENIUM_URL, $capabilities);
 
+        try {
             // Navigare către o pagină web
-            $driver->get('https://ro.betano.com/sport/fotbal/romania/liga-1/17088/');
-
+            $driver->get(self::BETANO_LIG1);
 
             // Așteaptă până când pagina este complet încărcată
-            $driver->wait(10)->until(
+            $driver->wait($waitTimeout)->until(
                 function ($driver) {
                     return $driver->executeScript('return document.readyState') === 'complete';
                 }
             );
 
             // Așteaptă până când elementele sunt prezente și vizibile pe pagină
-            $driver->wait(5)->until(
+            $driver->wait($waitPresenceTimeout)->until(
                 WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(WebDriverBy::className('vue-recycle-scroller__item-view'))
             );
-
-            // $wait = new WebDriverWait($driver, 5);
-            // $wait->until(
-            //     WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(WebDriverBy::className('vue-recycle-scroller__item-view'))
-            // );
 
             $matches = $driver->findElements(WebDriverBy::className('vue-recycle-scroller__item-view'));
             $betanoMatches = [];
             foreach ($matches as $match) {
                 $teamsElements = $match->findElements(WebDriverBy::className('tw-text-n-13-steel'));
-                //$teamName1Element = $match->findElement(WebDriverBy::xpath('.//a/div/div[1]/div[1]/span'));
                 $teamName1Element = $teamsElements[0];
                 $teamName1 = $teamName1Element->getText();
-        
+
                 $teamName2Element = $teamsElements[1];
                 $teamName2 = $teamName2Element->getText();
 
                 $key = "$teamName1-$teamName2";
 
-                $betDetails = ['team1Name' => '','team2Name' => '', '1' => '' , 'x' => '' , '2' => ''];
+                $betDetails = ['team1Name' => '', 'team2Name' => '', '1' => '', 'x' => '', '2' => ''];
 
                 $detailsBetElements = $match->findElements(WebDriverBy::className('tw-text-tertiary'));
                 $detailsBet1 = $detailsBetElements[0]->getText();
@@ -79,14 +81,12 @@ class SeleniumController extends Controller
                 $betanoMatches[$key] = $betDetails;
             }
 
-            // Închide driverul WebDriver
-            $driver->quit();
-            //dd($betanoMatches);
-            
-            return view('selenium', compact("betanoMatches"));
-        } catch (\Exception $e) {
-            $driver->quit();
-            dd($e);
+            return $betanoMatches;
+        } finally {
+            // Închide driverul WebDriver în blocul finally pentru a ne asigura că se închide întotdeauna
+            if (isset($driver)) {
+                $driver->quit();
+            }
         }
     }
 }
